753b92812c8490c5b3fcf3693c5ab0ad920762b2
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2023-01-22 20:24:26 -0300

    fix erro desafio 3 php 8.1

--- connectDatabase.php
+++ connectDatabase.php
@@ -20,28 +20,28 @@ function CreateDatabaseTableUsers($data)
   $pdo_connection = ConnectMysql($host, $port, $user, $password);
   try {
     $dummy_data = [
-      [1, "usuario1", "login1", "senha1"],
-      [2, "usuario2", "login2", "senha2"],
-      [3, "usuario3", "login3", "senha3"],
-      [4, "usuario4", "login4", "senha4"],
-      [5, "usuario5", "login5", "senha5"],
-      [6, "usuario6", "login6", "senha6"],
-      [7, "usuario7", "login7", "senha7"],
-      [8, "usuario8", "login8", "senha8"],
-      [9, "usuario9", "login9", "senha9"],
-      [10, "usuario10", "login10", "senha10"],
-      [11, "usuario11", "login11", "senha11"],
-      [12, "usuario12", "login12", "senha12"],
-      [13, "usuario13", "login13", "senha13"],
-      [14, "usuario14", "login14", "senha14"],
-      [15, "usuario15", "login15", "senha15"],
-      [16, "usuario16", "login16", "senha16"]
+      [1, "usuario1", "login1", "senha1", "usuario1"],
+      [2, "usuario2", "login2", "senha2", "usuario2"],
+      [3, "usuario3", "login3", "senha3", "usuario3"],
+      [4, "usuario4", "login4", "senha4", "usuario4"],
+      [5, "usuario5", "login5", "senha5", "usuario5"],
+      [6, "usuario6", "login6", "senha6", "usuario6"],
+      [7, "usuario7", "login7", "senha7", "usuario7"],
+      [8, "usuario8", "login8", "senha8", "usuario8"],
+      [9, "usuario9", "login9", "senha9", "usuario9"],
+      [10, "usuario10", "login10", "senha10", "usuario10"],
+      [11, "usuario11", "login11", "senha11", "usuario11"],
+      [12, "usuario12", "login12", "senha12", "usuario12"],
+      [13, "usuario13", "login13", "senha13", "usuario13"],
+      [14, "usuario14", "login14", "senha14", "usuario14"],
+      [15, "usuario15", "login15", "senha15", "usuario15"],
+      [16, "usuario16", "login16", "senha16", "usuario16"]
     ];
     $pdo_connection->query("CREATE DATABASE IF NOT EXISTS desafio");
     $pdo_connection->query("use desafio");
     $pdo_connection->query("CREATE TABLE IF NOT EXISTS usuarios (id INT NOT NULL AUTO_INCREMENT,
                             nome VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL, PRIMARY KEY (id))");
-    $insert = $pdo_connection->prepare("INSERT INTO usuarios (id, nome, login, senha) VALUES (?,?,?,?)");
+    $insert = $pdo_connection->prepare("INSERT INTO usuarios (id, nome, login, senha) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE nome=(?)");
     $pdo_connection->beginTransaction();
     foreach ($dummy_data as $row) {
       $insert->execute($row);
@@ -75,7 +75,6 @@ function GetUsersPaginate5($data)
   $query = $db->prepare("SELECT * FROM usuarios LIMIT 5 OFFSET " . $page);
   $query->execute(['offset' => $page]);
   $users = $query->fetchAll(PDO::FETCH_ASSOC);
-  // var_dump($users);
   if (!$users) return ["Nenhum dado encontrado!", "Resultado da consulta: "];
   $response = array_map(function ($user, $index) {
     if ($index == 0) return "id - nome - login - senha <br><br>" . join(" - ", $user);
